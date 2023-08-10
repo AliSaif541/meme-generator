0e75e998f09820162b9e505b3b61dcf814003c07
Ali Saif <muhammadalisaif2@gmail.com>
2023-08-11 01:45:17 +0500

    Adding Files

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <nav>
+            <img src="./images/troll-face.png" alt="logo" className='logo'></img>
+            <h2 className='Hheading'>Meme Generator</h2>
+            <p className='Htext'>React Course - Project 3</p>
+        </nav>
+    )
+}
